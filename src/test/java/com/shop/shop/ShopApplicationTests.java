4b8df0f10a0543c5720490a3ffe60b724f04ae17
poohoh <kjh980328@gmail.com>
2024-02-06 01:11:03 +0900

    Shop Project Start

--- src/test/java/com/shop/shop/ShopApplicationTests.java
+++ src/test/java/com/shop/shop/ShopApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.shop.shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringDemoApplicationTests {
+class ShopApplicationTests {
 
 	@Test
 	void contextLoads() {
