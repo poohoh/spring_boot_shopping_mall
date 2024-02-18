4c35c0b1df980c990a171548716d7d465c97722f
poohoh <kjh980328@gmail.com>
2024-02-19 00:27:52 +0900

    th:if, th:unless

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,6 +1,7 @@
 package com.shop.controller;
 
 import com.shop.dto.ItemDto;
+import com.shop.entity.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,5 +51,24 @@ public class ThymeleafExController {
         model.addAttribute("itemDtoList", itemDtoList);
 
         return "thymeleafEx/thymeleafEx03";
+    }
+
+    @GetMapping(value = "/ex04")
+    public String thymeleafExample04(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(1000 * i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+
+        return "thymeleafEx/thymeleafEx04";
     }
 }
