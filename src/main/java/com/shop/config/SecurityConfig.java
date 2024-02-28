ca7bee7921092b680aadac9552e68725eb77efff
poohoh <kjh980328@gmail.com>
2024-02-28 21:28:06 +0900

    4.5 페이지 권한 설정 (1)

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,13 +27,30 @@ public class SecurityConfig {
                 .loginPage("/members/login")  // 로그인 URL 설정
                 .defaultSuccessUrl("/")  //로그인 성공 시 이동할 URL
                 .usernameParameter("email")  // 로그인 시 사용할 파라미터 이름으로 email을 지정
-                .failureUrl("/members/login/error")  // 로그인 실패 시 이동할 URL
-                .and()
-                .logout()
+                .failureUrl("/members/login/error");  // 로그인 실패 시 이동할 URL
+
+        http.logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))  // 로그아웃 URL 설정
                 .logoutSuccessUrl("/");  // 로그아웃 성공 시 이동할 URL 설정
 
+        http.authorizeHttpRequests()
+                .mvcMatchers("/",
+                        "/members/**",
+                        "/item/**",
+                        "/images/**")
+                .permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
     @Bean
